3c7496fb19470a247ddce0fa98af337dab1559ec
asmina18 <asmina18@mail.ru>
2024-02-16 13:46:41 +0100

    update

--- code/src/Components/Navbar/Navbar.jsx
+++ code/src/Components/Navbar/Navbar.jsx
@@ -1,31 +1,38 @@
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
+export const Navbar = (props) => {
+  // Initialiserer tilstanden med en standardfarve eller en farve fra props
+  const [backgroundColor, setBackgroundColor] = useState(props.backgroundColor || "#F4A460");
 
-export const Navbar = ({  backgroundColor= "#F4A460" }) => {
+  // En funktion, der genererer en ny tilfældig farve og sætter den som baggrundsfarve
+  const changeBackgroundColor = () => {
+    const newColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    setBackgroundColor(newColor);
+  };
+
   const navStyle = {
-    backgroundColor,
+    backgroundColor, // Bruger tilstanden til at sætte baggrundsfarven
     padding: '10px',
     display: 'flex',
     justifyContent: 'space-around',
   };
-
- 
-  
 
   const navArr = [
     { Link: "/", Page: "Home"},
     { Link: "/contact", Page: "Contact" },
     { Link: "/about", Page: "About" },
     { Link: "/login", Page: "Login" },
-]
+  ];
 
   return (
     <nav style={navStyle}>
       {navArr.map((item, index) => (
-        <NavLink key={index} to={item.Link}>{item.Page} </NavLink>
+        // Tilføjer onClick-handler til hvert NavLink-element for at ændre farven
+        <NavLink key={index} to={item.Link} onClick={changeBackgroundColor} style={{ color: 'inherit', textDecoration: 'none' }}>
+          {item.Page}
+        </NavLink>
       ))}
     </nav>
-
   );
 };
-
